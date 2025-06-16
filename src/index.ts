14ef6bb6d13485143fcba6cb3d243ddfc4615dd9
simwai <16225108+simwai@users.noreply.github.com>
2025-06-16 10:16:24 +0200

    fix: :bug: Fixed that it always created an empty log folder on import

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { ConsoleLogger } from './logger/console-logger.js'
 import { FileLogger } from './logger/file-logger.js'
 import { Retry } from './retry.js'
 
-const consoleLogger = new ConsoleLogger({ isTimeEnabled: true })
-const fileLogger = new FileLogger({ logFilePath: 'logs/my-log.log' })
+const logger = new ConsoleLogger({ isTimeEnabled: true })
 const retry = new Retry()
 
 export { ConsoleLogger, FileLogger, Retry }
-export default { consoleLogger, retry, fileLogger }
+export default { logger, retry }
