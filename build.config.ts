9f3eeffd2b4373d0f4a7d21e9eb4269c8764361f
simwai <16225108+simwai@users.noreply.github.com>
2025-09-20 13:10:32 +0200

    build: :building_construction: Added tsconfig for test and adjusted the other configs to fully support ava testing

--- build.config.ts
+++ build.config.ts
@@ -2,34 +2,42 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig([
-	// Node.js build
+	// Node build
 	{
 		name: 'node',
 		entries: [{ input: './src/index', name: 'index' }],
 		declaration: true,
-		rollup: {
-			emitCJS: true,
-			output: { exports: 'named' },
-		},
+		rollup: { emitCJS: true, output: { exports: 'named' } },
 		outDir: 'build/node',
 		failOnWarn: false,
 	},
+
 	// Browser build
 	{
 		name: 'browser',
 		entries: [{ input: './src/index.browser', name: 'index' }],
 		declaration: true,
-		rollup: {
-			emitCJS: false,
-			output: { exports: 'named' },
-		},
+		rollup: { emitCJS: false, output: { exports: 'named' } },
 		hooks: {
-			'rollup:options'(_context, options) {
-				// Force bundling of all dependencies for browser
+			'rollup:options'(_ctx, options) {
 				options.external = () => false
 			},
 		},
 		outDir: 'build/browser',
+		failOnWarn: false,
+	},
+
+	// Tests build (auto-discovers files)
+	{
+		name: 'tests',
+		// Treat the directory as a multi-entry source
+		entries: [{ input: './src', builder: 'mkdist', ext: 'js' }],
+		outDir: './test-build',
+		declaration: false,
+		rollup: {
+			emitCJS: false,
+			output: { exports: 'named' },
+		},
 		failOnWarn: false,
 	},
 ])
