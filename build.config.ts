3a1cfbf5ca93dacbe945f4844b9d9830028ab8da
simwai <16225108+simwai@users.noreply.github.com>
2025-11-17 16:21:42 +0100

    build: :building_construction: Enabled tree-shaking in build config

--- build.config.ts
+++ build.config.ts
@@ -2,42 +2,44 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig([
-	// Node build
-	{
-		name: 'node',
-		entries: [{ input: './src/index', name: 'index' }],
-		declaration: true,
-		rollup: { emitCJS: true, output: { exports: 'named' } },
-		outDir: 'build/node',
-		failOnWarn: false,
-	},
+  // Node build
+  {
+    name: 'node',
+    entries: [{ input: './src/index', name: 'index' }],
+    declaration: true,
+    rollup: { emitCJS: true, output: { exports: 'named' }, esbuild: { treeShaking: true, charset: 'ascii' }, preserveDynamicImports: true },
+    outDir: './build/node',
+    failOnWarn: false,
+  },
 
-	// Browser build
-	{
-		name: 'browser',
-		entries: [{ input: './src/index.browser', name: 'index' }],
-		declaration: true,
-		rollup: { emitCJS: false, output: { exports: 'named' } },
-		hooks: {
-			'rollup:options'(_ctx, options) {
-				options.external = () => false
-			},
-		},
-		outDir: 'build/browser',
-		failOnWarn: false,
-	},
+  // Browser build
+  {
+    name: 'browser',
+    entries: [{ input: './src/index.browser', name: 'index' }],
+    declaration: true,
+    rollup: { emitCJS: false, output: { exports: 'named' }, esbuild: { treeShaking: true, charset: 'ascii' }, preserveDynamicImports: true },
+    hooks: {
+      'rollup:options'(_ctx, options) {
+        options.external = () => false
+      },
+    },
+    outDir: './build/browser',
+    failOnWarn: false,
+  },
 
-	// Tests build (auto-discovers files)
-	{
-		name: 'tests',
-		// Treat the directory as a multi-entry source
-		entries: [{ input: './src', builder: 'mkdist', ext: 'js' }],
-		outDir: './test-build',
-		declaration: false,
-		rollup: {
-			emitCJS: false,
-			output: { exports: 'named' },
-		},
-		failOnWarn: false,
-	},
+  // Tests build (auto-discovers files)
+  {
+    name: 'tests',
+    // Treat the directory as a multi-entry source
+    entries: [{ input: './src', builder: 'mkdist', ext: 'js' }],
+    outDir: './test-build',
+    declaration: false,
+    rollup: {
+      emitCJS: false,
+      esbuild: { treeShaking: true, charset: 'ascii' },
+      preserveDynamicImports: true,
+      output: { exports: 'named' },
+    },
+    failOnWarn: false,
+  },
 ])
